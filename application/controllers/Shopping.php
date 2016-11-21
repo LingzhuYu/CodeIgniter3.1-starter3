c3863d657113715171f250c11d7595761c4933ce
Injo Baik <baikinjo.28@gmail.com>
2016-11-20 19:05:04 -0800

    finished part b of the lab 8
    Signed-off-by: Injo Baik <baikinjo.28@gmail.com>

--- application/controllers/Shopping.php
+++ application/controllers/Shopping.php
@@ -19,6 +19,23 @@ class Shopping extends Application {
         else $this->summarize();
     }
     public function summarize() {
+        // identify all of the order files
+        $this->load->helper('directory');
+        $candidates = directory_map('../data/');
+        $parms = array();
+        foreach ($candidates as $filename) {
+            if (substr($filename,0,5) == 'order') {
+                // restore that order object
+                $order = new Order ('../data/' . $filename);
+                // setup view parameters
+                $parms[] = array(
+                    'number' => $order->number,
+                    'datetime' => $order->datetime,
+                    'total' => $order->total()
+                );
+            }
+        }
+        $this->data['orders'] = $parms;
         $this->data['pagebody'] = 'summary';
         $this->render('template');  // use the default template
     }
@@ -55,8 +72,8 @@ class Shopping extends Application {
 
     public function add($what) {
         $order = new Order($this->session->userdata('order'));
-        $this->session->set_userdata('order',(array)$order);
         $order->additem($what);
+        $this->session->set_userdata('order',(array)$order);
         $this->keep_shopping();
         redirect('/shopping');
     }
@@ -68,5 +85,22 @@ class Shopping extends Application {
         }
 
         $this->index();
+    }
+
+    public function checkout() {
+        $order = new Order($this->session->userdata('order'));
+        // ignore invalid requests
+        if (! $order->validate())
+            redirect('/shopping');
+        $order->save();
+        $this->session->unset_userdata('order');
+        redirect('/shopping');
+    }
+
+    public function examine($which) {
+        $order = new Order ('../data/order' . $which . '.xml');
+        $stuff = $order->receipt();
+        $this->data['content'] = $this->parsedown->parse($stuff);
+        $this->render();
     }
 }
